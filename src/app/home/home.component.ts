9ca3afd92016f3a0a877936c6acb0764c1f410e2
James Angelo Sarona <jacsarona.official@gmail.com>
2025-05-23 19:07:40 +0800

    fix: logout not working

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,21 +1,32 @@
 import { Component } from '@angular/core';
 import { Router, RouterModule } from '@angular/router';
 import { AuthService } from '../services/auth.service';
+import { NgIf } from '@angular/common'; 
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css'],
   standalone: true,
-  imports: [RouterModule],
+  imports: [RouterModule, NgIf],
 })
 export class HomeComponent {
+  isLoggingOut = false;
+  logoutError = '';
+
   constructor(private auth: AuthService, private router: Router) {}
 
   logout() {
+    this.isLoggingOut = true;
+    this.logoutError = '';
     this.auth.logout().subscribe({
-      next: () => this.router.navigate(['/login']),
-      error: () => {
+      next: () => {
+        this.isLoggingOut = false;
+        this.router.navigate(['/login']);
+      },
+      error: (err) => {
+        this.isLoggingOut = false;
+        this.logoutError = 'Logout failed. Redirecting...';
         this.router.navigate(['/login']);
       },
     });
