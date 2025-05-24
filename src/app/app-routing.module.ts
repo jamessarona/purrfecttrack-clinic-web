0dbf036b822d150809c7104a950a4342927ebef8
James Angelo <jacsarona.official@gmail.com>
2025-05-25 02:53:47 +0800

    feat: configure base layout children

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,17 @@
 import { Routes } from '@angular/router';
 import { LoginComponent } from './pages/login/login.component';
 import { HomeComponent } from './pages/home/home.component';
+import { BaseLayoutComponent } from './layouts/base-layout/base-layout.component';
 
 export const appRoutes: Routes = [
   { path: '', redirectTo: 'login', pathMatch: 'full' },
   { path: 'login', component: LoginComponent },
-  { path: 'home', component: HomeComponent }
+  {
+    path: '',
+    component: BaseLayoutComponent,
+    children: [
+      { path: 'home', component: HomeComponent },
+      // { path: 'pet-record', component: PetRecordComponent },
+    ]
+  }
 ];
