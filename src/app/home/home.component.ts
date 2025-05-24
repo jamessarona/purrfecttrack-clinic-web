59af8f04d075397e02d776bc3a77ee1072882ed4
James Angelo <jacsarona.official@gmail.com>
2025-05-24 21:21:49 +0800

    feat: implement navbar in homepage

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -2,13 +2,14 @@ import { Component } from '@angular/core';
 import { Router, RouterModule } from '@angular/router';
 import { AuthService } from '../services/auth.service';
 import { NgIf } from '@angular/common'; 
+import { NavbarComponent } from '../components/navbar/navbar.component';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css'],
   standalone: true,
-  imports: [RouterModule, NgIf],
+  imports: [RouterModule, NgIf, NavbarComponent],
 })
 export class HomeComponent {
   isLoggingOut = false;
