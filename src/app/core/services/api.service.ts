a55857d2fd92b957556fb66cf2d0426ad3b87651
James Angelo Sarona <jacsarona.official@gmail.com>
2025-05-28 00:04:00 +0800

    refactor: fix api service to add with credentials

--- src/app/core/services/api.service.ts
+++ src/app/core/services/api.service.ts
@@ -10,20 +10,20 @@ export class ApiService {
   private readonly baseUrl = environment.apiBaseUrl;
 
   constructor(private http: HttpClient) {}
-
+  
   get<T>(endpoint: string): Observable<T> {
-    return this.http.get<T>(`${this.baseUrl}/${endpoint}`);
+    return this.http.get<T>(`${this.baseUrl}/${endpoint}`, { withCredentials: true });
   }
-
+  
   post<T>(endpoint: string, data: any): Observable<T> {
-    return this.http.post<T>(`${this.baseUrl}/${endpoint}`, data);
+    return this.http.post<T>(`${this.baseUrl}/${endpoint}`, data, { withCredentials: true });
   }
-
+  
   put<T>(endpoint: string, data: any): Observable<T> {
-    return this.http.put<T>(`${this.baseUrl}/${endpoint}`, data);
+    return this.http.put<T>(`${this.baseUrl}/${endpoint}`, data, { withCredentials: true });
   }
-
+  
   delete<T>(endpoint: string): Observable<T> {
-    return this.http.delete<T>(`${this.baseUrl}/${endpoint}`);
+    return this.http.delete<T>(`${this.baseUrl}/${endpoint}`, { withCredentials: true });
   }
 }
