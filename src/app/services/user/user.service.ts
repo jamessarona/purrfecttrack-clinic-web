d4265a763a39ce5500421c305700dc620fb7c98e
James Angelo <jacsarona.official@gmail.com>
2025-06-10 22:16:57 +0800

    feat: add update user detail

--- src/app/services/user/user.service.ts
+++ src/app/services/user/user.service.ts
@@ -2,12 +2,17 @@ import { Injectable } from '@angular/core';
 import { BehaviorSubject, Observable, catchError, tap, throwError } from 'rxjs';
 import { UserDetailModel } from '../../core/models/user-detail.model';
 import { ApiService } from '../../core/services/api.service';
+import { VetModel } from '../../core/models/vet.model';
+import { VetStaffModel } from '../../core/models/vet-staff.model';
+import { UserRole } from '../../core/models/user-role.enum';
 
 @Injectable({
   providedIn: 'root'
 })
 export class UserService {
   private readonly API_CURRENT_USER_DETAIL = 'users/current';
+  private static readonly VET_ENDPOINT = 'vet';
+  private static readonly VETSTAFF_ENDPOINT = 'vetStaff';
 
   private userSubject = new BehaviorSubject<UserDetailModel | null>(null);
   user$ = this.userSubject.asObservable();
@@ -30,5 +35,44 @@ export class UserService {
 
   clearUser(): void {
     this.userSubject.next(null);
+  }
+
+
+  updateUserDetails(updatedData: { vet?: VetModel | null; vetStaff?: VetStaffModel | null }): Observable<any> {
+    const user = this.getUser();
+    if (!user) {
+      return throwError(() => new Error('User not loaded'));
+    }
+
+    switch (user.role) {
+      case UserRole.Vet:
+        return this.updateVet(user, updatedData.vet);
+      case UserRole.VetStaff:
+        return this.updateVetStaff(user, updatedData.vetStaff);
+      default:
+        return throwError(() => new Error('User role not authorized to update details'));
+    }
+  }
+
+  private updateVet(user: UserDetailModel, vetData?: VetModel | null): Observable<any> {
+    if (!vetData) {
+      return throwError(() => new Error('No Vet data provided'));
+    }
+    const vetId = user.vet?.id;
+    if (!vetId) {
+      return throwError(() => new Error('Vet ID missing'));
+    }
+    return this.api.put(`${UserService.VET_ENDPOINT}/${vetId}`, vetData);
+  }
+
+  private updateVetStaff(user: UserDetailModel, vetStaffData?: VetStaffModel | null): Observable<any> {
+    if (!vetStaffData) {
+      return throwError(() => new Error('No VetStaff data provided'));
+    }
+    const vetStaffId = user.vetStaff?.id;
+    if (!vetStaffId) {
+      return throwError(() => new Error('VetStaff ID missing'));
+    }
+    return this.api.put(`${UserService.VETSTAFF_ENDPOINT}/${vetStaffId}`, vetStaffData);
   }
 }
